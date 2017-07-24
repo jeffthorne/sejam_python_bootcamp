3fcebdc0791c49368f5eaf82002e4aa4f70c9253
Jeff Thorne <jeff.thorne@gmail.com>
2017-07-24 17:35:28 -0400

    added ds api use case 1

---
+++ 7_dsp3/a_overview.py
@@ -0,0 +1,31 @@
+'Deep Security Python 3 SDK -> DSP3'
+
+'''
+Goals
+* To provide a simple to use interface written in Python 3 that provides and abstraction layer over
+  both the DS REST and SOAP API calls
+* Simple to use for folks with litte or now programmig experience
+
+Installation
+pip install -i https://testpypi.python.org/pypi dsp3
+'''
+
+from dsp3.models.manager import Manager       #step one import base Manager class
+
+dsm = Manager(tenant="tenant",username="username", password="password")
+
+#lets check the api version
+print(dsm.get_api_version())
+
+
+#lets get Malware events over last 24 hours
+events = dsm.antimalware_event_retreive(time_type="LAST_24_HOURS")
+
+if events['antiMalwareEvents']:
+	for event in events['antiMalwareEvents'][0]:
+		print('Malware name:', event['malwareName'])    
+
+
+
+#make sure to end connections as they are limited.	
+dsm.end_session()
---
+++ 7_dsp3/b_api_use_case_1.py
@@ -0,0 +1,81 @@
+'DS API Use Case 1: Add 300 AWS accounts in DS'
+
+'''
+path: /rest/cloudaccounts/aws      methods=[POST]
+
+Adds a new AWS cloud account into Deep Security
+Input:
+AddAwsAccountRequest - AddAwsAccountRequest object describing the Amazon account.
+
+Output:
+AddAwsAccountResponse - A response with status 200 OK and a body containing AddAwsAccountResponse object when addition was successful If there is an authentication issue with the session cookie it will return a 403 Forbidden If there is a permission issue with the give AWS credentials, or the account has already been imported it will return a 400 Bad Request If there is an error adding the account it will return a 500 Internal Server Error
+
+Cookie parameters:
+sID - a session ID returned by the Authentication API.
+
+Produces:
+application/json
+
+Consumes:
+application/json
+
+
+
+Name: AddAwsAccountRequest
+Request object for the AWS Account API.
+
+JSON Example:
+{"AddAwsAccountRequest":
+ {
+   "awsCredentials": awsCredentials,
+   "crossAccountRole": crossAccountRole,
+   "seedRegion": String,
+   "useInstanceRole": Boolean,
+ }
+}
+
+
+
+Name: awsCredentials
+Credentials object
+{"awsCredentials":
+ {
+   "accessKeyId": String,
+   "secretKey": String,
+ }
+}
+
+
+Name: crossAccountRole
+Cross Account Role object
+
+JSON Example:
+{"crossAccountRole":
+ {
+   "externalId": String,
+   "roleArn": String,
+ }
+}
+
+
+'''
+import sys
+import os
+sys.path.append(os.path.join(os.path.dirname(__file__), '..', '..'))
+
+from python_bootcamp import config
+
+from dsp3.models.manager import Manager
+
+dsm = Manager(username=config.DSAS_USER, password=config.DSAS_PASSWORD, tenant=config.DSAS_TENANT)
+#dsm.add_aws_cloud_account_with_keys('access_key', 'secret_key')
+
+# or
+
+print( dsm.add_aws_cloud_account_with_cross_account_role(config.EXTERNAL_ID, config.ARN)) 
+dsm.end_session()
+# View DSPS Implentation -> https://github.com/jeffthorne/DSP3/blob/master/dsp3/utilities/cloudacct_utils.py
+
+
+
+
