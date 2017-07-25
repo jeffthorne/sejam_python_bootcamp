8eb8ed1b054cdce6e0a401ad2f3f72cea2751738
Jeff Thorne <jeff.thorne@gmail.com>
2017-07-25 10:24:04 -0400

    adding config file

--- 7_dsp3/c_secure_devops_pipeline.py
+++ 7_dsp3/c_secure_devops_pipeline.py
@@ -1,7 +1,1 @@
 'Secure Devops Pipeline'
-
-'''
-We will go over deploying a python web applicaiton to Docker containers in AWS
-using Ansible with DS integrated App Control toggles, Github, etc
-
-'''
